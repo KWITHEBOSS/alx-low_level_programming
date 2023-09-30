c264049fdf429ec097f62968daf6829dac95464c
KWITHEBOSS <happypromesse@tutanota.com>
2023-09-30 12:57:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print the alphabest 10 times
- * Return: void here
+ * print_alphabet - print all alphabet in lowercase
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	char c;
-	int i = 0;
+	char letter;
 
-		while (i <= 9)
-		{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-			_putchar('\n');
-			i++;
-		}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
