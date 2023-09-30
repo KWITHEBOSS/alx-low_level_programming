3de1b428099d97533d8a5b676bb84d2d6e38ddfd
KWITHEBOSS <happypromesse@tutanota.com>
2023-09-30 15:25:53 -0700

    nm libmy.a

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,28 +1,16 @@
 #include "main.h"
+
 /**
- * _atoi - convert a string into an integer.
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to be checked
  *
- * @s: the string to use.
- *
- * Return: integer.
+ * Return: 0 or 1
  */
-int _atoi(char *s)
+
+int _isdigit(int c)
 {
- 	int sign = 1, i = 0;
- 	unsigned int res = 0;
-
-
- 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
- 	{
- 		if (s[i] == '-')
- 			sign *= -1;
- 		i++;
- 	}
- 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
- 	{
- 		res = (res * 10) + (s[i] - '0');
- 		i++;
- 	}
- 	res *= sign;
- 	return (res);
- }
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
