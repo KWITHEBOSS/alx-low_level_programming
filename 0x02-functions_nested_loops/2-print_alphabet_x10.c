6782b66b68e2d70a6128df70d1edac319ef65feb
KWITHEBOSS <happypromesse@tutanota.com>
2023-09-30 12:46:56 -0700

    dior

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,36 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-	print_alphabet();
+	print_alphabet_x10();
 	return (0);
-}
-
-Question 2.
-
-#include "main.h"
-
-/**
- * print_alphabet_x10 - print the alphabest 10 times
- * Return: void here
- */
-
-void print_alphabet_x10(void)
-{
-	char c;
-	int i = 0;
-
-		while (i <= 9)
-		{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
-			_putchar('\n');
-			i++;
-		}
 }
